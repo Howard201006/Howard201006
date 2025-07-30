fc0f89450b6ec4cf7aeef205e62ceb9fecc1842a
薛宏丞 <howard201006@gmail.com>
2025-07-30 03:19:12 +0000

    123

--- lesson7/lesson7_3.py
+++ lesson7/lesson7_3.py
@@ -1,7 +1,7 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="要求使用者輸入姓名")
-parser.add_arument("name",help"請輸入姓名")
+parser.add_argument("--n","--name",type=str,help="姓名")
 args =  parser.parse_args()
 
 print(f"您的姓名是:{args.name}")
